4b96302cbccc328b87d2b5460a795b0f8f023089
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-04-09 01:44:41 +0530

    Listing page completed

--- Client/src/pages/PropertyDetails.jsx
+++ Client/src/pages/PropertyDetails.jsx
@@ -90,7 +90,7 @@ const PropertyDetails = () => {
               {/* form check availability  */}
               <form
                 action=""
-                className="text-gray-500  rounded-lg px-6 py-4 flex flex-col lg:flex-row gap-4  max-w-md lg:max-w-full ring-1 ring-slate-900/5 relative mt-10"
+                className="text-gray-500 bg-secondary/10   rounded-lg px-6 py-4 flex flex-col lg:flex-row gap-4  max-w-md lg:max-w-full ring-1 ring-slate-900/5 relative mt-10"
               >
                 <div className="flex flex-col w-full">
                   <div className="flex items-center gap-2">
@@ -167,7 +167,7 @@ const PropertyDetails = () => {
             {/* right side  */}
 
             <div className="flex-1 max-w-sm">
-                <div>
+                <div className="p-6 rounded-xl border border-slate-900/10">
                     <h4 className="h4 mb-3">Contact Agent</h4>
                     <form className="flex flex-col gap-4">
                         <input type="text" placeholder="Your name" className="p-2 py-1 border border-gray-300 rounded-md text-sm required" />
@@ -175,20 +175,54 @@ const PropertyDetails = () => {
                         <textarea rows={4}  placeholder="Your Message" className="p-2 py-1 border border-gray-300 rounded-md text-sm required" />
                         <button type="submit" className="btn-secondary rounded-lg py-1.5">Send Message</button>
                     </form>
-                    <h4 className="h4 mb-3 mt-8">
-                        <div>
-                            <div>
+                    <h4 className="h4 mb-3 mt-8">For Buying Contact</h4>
+                        <div className="text-sm w-80 divide-y divide-gray-500/30 border border-gray-500/30 rounded">
+                            <div className="flex items-start justify-between p-3">
                                 <div>
-                                    <div>
-                                        <h5>{property.agency.name}</h5>
-                                        <p>Agency</p>
+                                    <div className="flex items-center space-x-2">
+                                        <h5 className="h5">{property.agency.name}</h5>
+                                        <p className="bg-green-500/20 px-2 py-0.5 rounded-full text-xs text-green-600 border border-green-500/30">Agency</p>
                                     </div>
                                     <p>Agency office</p>
                                 </div>
-                                <img src={assets.userImg} alt="" />
+                                <img src={property.agency.owner.image} alt="" className="h-10 w-10 rounded-e-full" />
                             </div>
+
+                            <div className="flexStart gap-2 p-1.5">
+                            <div className="bg-green-500/20 p-1 rounded-full border border-green-500/30">
+                                <img src={assets.phone} alt="" width={14} />
+                            </div>
+                            <p>{property.agency.contact}</p>
+                            
+                                
+                            </div>
+
+                            <div className="flexStart gap-2 p-1.5">
+                            <div className="bg-green-500/20 p-1 rounded-full border border-green-500/30">
+                                <img src={assets.phone} alt="" width={14} />
+                            </div>
+                            <p>{property.agency.email}</p>
+                            
+                                
+                            </div>
+
+                            <div className="flex items-center divide-gray-500/30">
+                                <button className="flex items-center justify-center gap-2 w-1/2 py-3 cursor-pointer">
+                                      <img src={assets.mail} alt="" width={19} />
+                                      Send Email
+                                </button>
+
+                                <button className="flex items-center justify-center gap-2 w-1/2 py-3 cursor-pointer">
+                                      <img src={assets.phone} alt="" width={19} />
+                                      Call Now
+                                </button>
+                            </div>
+
+                            
+
+
                         </div>
-                    </h4>
+                    
                 </div>
             </div>
 
